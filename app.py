ec86a6d032f4e31adf35568ad0aa2dce48bf29a0
Dasari surya sricharan <2300520143@kluniversity.in>
2026-03-09 12:40:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -884,6 +884,6 @@ if __name__ == "__main__":
     print("=" * 50 + "\n")
     app.run(debug=False)
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
